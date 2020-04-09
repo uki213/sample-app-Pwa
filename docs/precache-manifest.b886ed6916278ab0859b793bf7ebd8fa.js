8c0e817005263f6ca05e11a86b152d70038df07f
Niimi Youki <niimiyouki@gmail.com>
2020-04-09 12:52:58 +0900

    追加

--- docs/precache-manifest.b886ed6916278ab0859b793bf7ebd8fa.js
+++ docs/precache-manifest.b886ed6916278ab0859b793bf7ebd8fa.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3828a840da9cb7d03bf8",
-    "url": "css/app.c6175a8e.css"
+    revision: '3828a840da9cb7d03bf8',
+    url: 'css/app.c6175a8e.css',
   },
   {
-    "revision": "82b9c7a5a3f405032b1db71a25f67021",
-    "url": "img/logo.82b9c7a5.png"
+    revision: '82b9c7a5a3f405032b1db71a25f67021',
+    url: 'img/logo.82b9c7a5.png',
   },
   {
-    "revision": "77535313e7a37c773860a2cceedca894",
-    "url": "index.html"
+    revision: '77535313e7a37c773860a2cceedca894',
+    url: 'index.html',
   },
   {
-    "revision": "baa27e166950c0dd787a",
-    "url": "js/about.ec5d2256.js"
+    revision: 'baa27e166950c0dd787a',
+    url: 'js/about.ec5d2256.js',
   },
   {
-    "revision": "3828a840da9cb7d03bf8",
-    "url": "js/app.71bdbcb6.js"
+    revision: '3828a840da9cb7d03bf8',
+    url: 'js/app.71bdbcb6.js',
   },
   {
-    "revision": "267ee6c50163a6bc1793",
-    "url": "js/chunk-vendors.608688aa.js"
+    revision: '267ee6c50163a6bc1793',
+    url: 'js/chunk-vendors.608688aa.js',
   },
   {
-    "revision": "cc6d40fa010a50c416b1eb2a286df54e",
-    "url": "manifest.json"
+    revision: 'cc6d40fa010a50c416b1eb2a286df54e',
+    url: 'manifest.json',
   },
   {
-    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "robots.txt"
-  }
+    revision: 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    url: 'robots.txt',
+  },
 ]);
